372f6bc264be41628d9079a3d511ca906bf4d16b
Piero Toffanin <pt@masseranolabs.com>
2020-04-06 10:52:07 -0400

    Add CRS to GeoJSON output

--- libs/entry.cpp
+++ libs/entry.cpp
@@ -278,6 +278,11 @@ int BasicGeometry::size() const{
 
 void BasicGeometry::initGeoJsonBase(json &j) const{
     j["type"] = "Feature";
+    j["crs"] = json();
+    j["crs"]["type"] = "name";
+    j["crs"]["properties"] = json();
+    j["crs"]["properties"]["name"] = "EPSG:4326";
+
     j["geometry"] = json({});
     j["properties"] = json({});
 }
