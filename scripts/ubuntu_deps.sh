5622dfcd8daeafb04276259b6b1cef6e698f5ccd
Piero Toffanin <pt@masseranolabs.com>
2020-06-24 15:52:00 +0000

    Use ubuntugis/unstable for GDAL >= 3

--- scripts/ubuntu_deps.sh
+++ scripts/ubuntu_deps.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install -y --fix-missing --no-install-recommends build-essential ca-certificates cmake git sqlite3 spatialite-bin exiv2 libexiv2-dev libgeos-dev libgdal-dev g++-8 gcc-8
+sudo apt update && sudo apt install -y --fix-missing --no-install-recommends build-essential
+sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable
+sudo apt install -y --fix-missing --no-install-recommends ca-certificates cmake git sqlite3 spatialite-bin exiv2 libexiv2-dev libgeos-dev libgdal-dev g++-8 gcc-8
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 800 --slave /usr/bin/g++ g++ /usr/bin/g++-8
