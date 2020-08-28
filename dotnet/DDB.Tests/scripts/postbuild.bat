6749bbfad26a02ce0e52fce35a142494209dc74a
Piero Toffanin <pt@masseranolabs.com>
2020-08-28 14:18:03 -0400

    post build script

---
+++ dotnet/DDB.Tests/scripts/postbuild.bat
@@ -0,0 +1,14 @@
+echo Copying DLL to output directory
+
+set TARGETDIR=%1
+if "%DDB_LIB_PATH%"=="" (SET COPY_FROM=..\..\build) ELSE (SET COPY_FROM="%DDB_LIB_PATH%") 
+
+if NOT EXIST "%COPY_FROM%\ddb.dll" GOTO :NODIR
+
+xcopy "%COPY_FROM%\*.*" "%TARGETDIR%" /i /d /y
+
+exit 0
+
+:NODIR
+echo Error! No ddb.dll found in %COPY_FROM%. Build ddb.dll first, then try again. You can also set DDB_LIB_PATH to a different path if you've built the library elsewhere.
+exit 1
