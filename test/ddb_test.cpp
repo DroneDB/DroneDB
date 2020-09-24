2c0886d9fa71a08932e982a6fdba97c0b330d0ff
Luca Di Leo <ldileo@digipa.it>
2020-09-24 18:58:53 +0200

    Added first remove test

--- test/ddb_test.cpp
+++ test/ddb_test.cpp
@@ -5,6 +5,8 @@
 #include "gtest/gtest.h"
 #include "dbops.h"
 #include "exceptions.h"
+#include "test.h"
+#include "testarea.h"
 
 namespace {
 
@@ -40,5 +42,19 @@ TEST(getIndexPathList, dontIncludeDirs) {
     EXPECT_STREQ(pathList[0].string().c_str(), (fs::path("data") / "folderA" / "test.txt").string().c_str());
 
 }
+
+TEST(deleteFromIndex, simplePath) {
+    TestArea ta(TEST_NAME);
+
+    const auto sqlite = ta.downloadTestAsset("https://github.com/DroneDB/test_data/raw/master/ddb-remove-test/.ddb/dbase.sqlite", "dbase.sqlite");
+
+    const auto testFolder = ta.getFolder("test");
+    create_directory(testFolder / ".ddb");
+    fs::copy(sqlite.string(), testFolder / ".ddb");
+    EXPECT_TRUE(fs::exists(testFolder / ".ddb" / "dbase.sqlite"));
+
+	// TODO: Finish here
+}
+
 
 }
