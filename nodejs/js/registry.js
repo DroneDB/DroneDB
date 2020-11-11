862423f9ae5293f9a2196f2152970bbd5d21a2d2
Piero Toffanin <pt@masseranolabs.com>
2020-11-11 16:12:33 -0500

    Registry login

--- nodejs/js/registry.js
+++ nodejs/js/registry.js
@@ -4,14 +4,39 @@
 
 const localStorage = require('../polyfills/node/localStorage');
 const fetch = require('../polyfills/node/fetch');
+const FormData = require('../polyfills/node/FormData');
 
 module.exports = class Registry{
     constructor(url){
         this.url = url;
     }
 
-    // TODO: move login here
-    // TODO: add refresh token endpoint
+    async login(username, password){
+        const formData = new FormData();
+        formData.append("username", username);
+        formData.append("password", password);
+
+        try{
+            const res = await fetch(`${this.url}/users/authenticate`, {
+                method: 'POST',
+                body: formData
+            }).then(r => r.json());
+            
+            if (res.token){
+                this.setCredentials(username, res.token, res.expires);
+
+                return res.token;
+            }else{
+                throw new Error(res.error || `Cannot login: ${JSON.stringify(res)}`);
+            }
+        }catch(e){
+            throw new Error(`Cannot login: ${e.message}`);
+        }
+    }
+
+    logout(){
+        this.clearCredentials();
+    }
 
     setCredentials(username, token, expires){
         localStorage.setItem(`${this.url}_username`, username);
