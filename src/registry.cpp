c28f08893d606863fb70ec53d4ec637fd95c8fe8
Luca Di Leo <ldileo@digipa.it>
2020-11-11 16:04:22 +0100

    Format and cleanup

--- src/registry.cpp
+++ src/registry.cpp
@@ -2,70 +2,77 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "url.h"
-#include "utils.h"
-#include "net.h"
-#include "json.h"
-#include "exceptions.h"
 #include "registry.h"
+
+#include "exceptions.h"
+#include "json.h"
+#include "net.h"
+#include "url.h"
 #include "userprofile.h"
+#include "utils.h"
 
 using homer6::Url;
 
-namespace ddb{
+namespace ddb {
 
-Registry::Registry(const std::string &url){
+Registry::Registry(const std::string &url) {
     std::string urlStr = url;
     if (urlStr.empty()) urlStr = std::string(DEFAULT_REGISTRY);
 
     Url u;
 
     // Always append https if no protocol is specified
-    if (urlStr.find("https://") != 0 && urlStr.find("http://") != 0){
+    if (urlStr.find("https://") != 0 && urlStr.find("http://") != 0) {
         u.fromString("https://" + urlStr);
-    }else{
+    } else {
         u.fromString(urlStr);
     }
 
     // Validate and set URL
-    if (u.getScheme() != "https" && u.getScheme() != "http"){
+    if (u.getScheme() != "https" && u.getScheme() != "http") {
         throw URLException("Registry URL can only be http/https");
     }
 
-    std::string port = u.getPort() != 80 && u.getPort() != 443 ? ":" + std::to_string(u.getPort()) : "";
+    const std::string port = u.getPort() != 80 && u.getPort() != 443
+                                 ? ":" + std::to_string(u.getPort())
+                                 : "";
     this->url = u.getScheme() + "://" + u.getHost() + port + u.getPath();
 
     LOGD << "Registry URL: " << this->url;
-
 }
 
-std::string Registry::getUrl(const std::string &path) const{
+std::string Registry::getUrl(const std::string &path) const {
     return url + path;
 }
 
-std::string Registry::login(const std::string &username, const std::string &password) const{
-    net::Response res = net::POST(getUrl("/users/authenticate"))
-                            .formData({"username", username, "password", password})
-                            .send();
+std::string Registry::login(const std::string &username,
+                            const std::string &password) const {
+    net::Response res =
+        net::POST(getUrl("/users/authenticate"))
+            .formData({"username", username, "password", password})
+            .send();
 
     json j = res.getJSON();
 
-    if (res.status() == 200 && j.contains("token")){
-        std::string token = j["token"].get<std::string>();
+    if (res.status() == 200 && j.contains("token")) {
+        auto token = j["token"].get<std::string>();
 
         // Save for next time
-        UserProfile::get()->getAuthManager()->saveCredentials(url, AuthCredentials(username, password));
+        UserProfile::get()->getAuthManager()->saveCredentials(
+            url, AuthCredentials(username, password));
 
         return token;
-    }else if (j.contains("error")){
-        throw AuthException("Login failed: " + j["error"].get<std::string>());
-    }else{
-        throw AuthException("Login failed: host returned " + std::to_string(res.status()));
     }
+    if (j.contains("error")) {
+        throw AuthException("Login failed: " + j["error"].get<std::string>());
+    }
+
+    throw AuthException("Login failed: host returned " +
+                        std::to_string(res.status()));
 }
 
-bool Registry::logout(){
+bool Registry::logout() {
     return UserProfile::get()->getAuthManager()->deleteCredentials(url);
 }
 
-}
+}  // namespace ddb
