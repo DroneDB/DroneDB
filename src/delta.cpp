1926c01de9b3e46686492558f2dae5dfac7cde64
Luca Di Leo <admin@lucadileo.it>
2021-02-11 11:24:12 +0100

    Fix delta between same tree

--- src/delta.cpp
+++ src/delta.cpp
@@ -4,16 +4,15 @@
 
 #include <mio.h>
 
+#include <algorithm>
 #include <optional>
 #include <utility>
-#include <algorithm>
 #include <vector>
 
 #include "dbops.h"
 #include "exceptions.h"
 
 namespace ddb {
-
 
 struct SimpleEntry {
     std::string path;
@@ -30,7 +29,6 @@ struct SimpleEntry {
         this->hash = std::move(hash);
         this->type = type;
     }
-
 };
 
 struct CopyAction {
@@ -45,7 +43,6 @@ struct CopyAction {
     std::string toString() const {
         return "CPY -> " + source + " TO " + destination;
     }
-
 };
 
 struct RemoveAction {
@@ -61,7 +58,6 @@ struct RemoveAction {
         return "DEL -> [" + std::to_string(static_cast<int>(type)) + "] " +
                path;
     }
-
 };
 
 struct AddAction {
@@ -80,7 +76,6 @@ struct AddAction {
 };
 
 std::vector<SimpleEntry> getAllSimpleEntries(Database* db) {
-
     auto q = db->query("SELECT path, hash, type FROM entries");
 
     std::vector<SimpleEntry> entries;
@@ -135,30 +130,40 @@ void delta(Database* sourceDb, Database* targetDb, std::ostream& output,
               });
 
     for (const SimpleEntry& entry : source) {
+        
+        const auto inDestWithSameHashAndPath =
+            std::find_if(destination.begin(), destination.end(),
+                         [&entry](const SimpleEntry& e) {
+                             return e.hash == entry.hash &&
+                                    e.type == entry.type &&
+                                    e.path == entry.path;
+                         }) == destination.end();
+
+        if (inDestWithSameHashAndPath) {
+            LOGD << "SKIP -> " << entry.toString();
+            continue;
+        }
+
         const auto inDestWithSameHashEntry = std::find_if(
             destination.begin(), destination.end(),
             [&entry](const SimpleEntry& e) {
                 return e.hash == entry.hash && e.type == entry.type;
             });
 
-        if (inDestWithSameHashEntry != destination.end() &&
-            inDestWithSameHashEntry->path == entry.path) {
-            LOGD << "SKIP -> " << entry.toString();
+        if (inDestWithSameHashEntry == destination.end()) {
+            LOGD << "ADD  -> " << entry.toString();
+            adds.emplace_back(AddAction(entry.path, entry.type));
+            continue;
+        }
+        
+        if (inDestWithSameHashEntry->type != Directory) {
+            LOGD << "COPY -> " << inDestWithSameHashEntry->toString() << " =>"
+                 << entry.toString();
+            copies.emplace_back(
+                CopyAction(inDestWithSameHashEntry->path, entry.path));
         } else {
-            if (inDestWithSameHashEntry == destination.end()) {
-                LOGD << "ADD  -> " << entry.toString();
-                adds.emplace_back(AddAction(entry.path, entry.type));
-            } else {
-                if (inDestWithSameHashEntry->type != Directory) {
-                    LOGD << "COPY -> " << inDestWithSameHashEntry->toString()
-                         << " =>" << entry.toString();
-                    copies.emplace_back(
-                        CopyAction(inDestWithSameHashEntry->path, entry.path));
-                } else {
-                    LOGD << "ADD FOLDER -> " << entry.toString();
-                    adds.emplace_back(AddAction(entry.path, entry.type));
-                }
-            }
+            LOGD << "ADD FOLDER -> " << entry.toString();
+            adds.emplace_back(AddAction(entry.path, entry.type));
         }
     }
 
@@ -180,7 +185,6 @@ void delta(Database* sourceDb, Database* targetDb, std::ostream& output,
     } else if (format == "text") {
         // todo
     }
-
 }
 
 }  // namespace ddb
