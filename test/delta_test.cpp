18c9ef11132b2782446dfe1b342883d7b24ef3b1
Luca Di Leo <ldileo@digipa.it>
2021-02-11 20:04:09 +0100

    Added some tests

---
+++ test/delta_test.cpp
@@ -0,0 +1,80 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include <delta.h>
+
+#include "dbops.h"
+#include "exceptions.h"
+#include "gtest/gtest.h"
+#include "test.h"
+#include "testarea.h"
+
+namespace {
+
+using namespace ddb;
+
+TEST(deltaList, simpleAdd) {
+    const std::vector<SimpleEntry> dest{SimpleEntry("1.jpg", "AAA", Generic),
+                                        SimpleEntry("2.jpg", "BBB", Generic),
+                                        SimpleEntry("3.jpg", "CCC", Generic)};
+
+    const std::vector<SimpleEntry> source{SimpleEntry("1.jpg", "AAA", Generic),
+                                          SimpleEntry("2.jpg", "BBB", Generic),
+                                          SimpleEntry("3.jpg", "CCC", Generic),
+                                          SimpleEntry("4.jpg", "DDD", Generic)};
+
+    const auto delta = getDelta(source, dest);
+
+    EXPECT_EQ(delta.copies.size(), 0);
+    EXPECT_EQ(delta.adds.size(), 1);
+    EXPECT_EQ(delta.removes.size(), 0);
+
+    EXPECT_EQ(delta.adds[0].path, "4.jpg");
+    EXPECT_EQ(delta.adds[0].type, Generic);
+}
+
+TEST(deltaList, simpleRemove) {
+    const std::vector<SimpleEntry> dest{SimpleEntry("1.jpg", "AAA", Generic),
+                                        SimpleEntry("2.jpg", "BBB", Generic),
+                                        SimpleEntry("3.jpg", "CCC", Generic)};
+
+    const std::vector<SimpleEntry> source{
+        SimpleEntry("1.jpg", "AAA", Generic),
+        SimpleEntry("2.jpg", "BBB", Generic),
+    };
+
+    const auto delta = getDelta(source, dest);
+
+    EXPECT_EQ(delta.copies.size(), 0);
+    EXPECT_EQ(delta.adds.size(), 0);
+    EXPECT_EQ(delta.removes.size(), 1);
+
+    EXPECT_EQ(delta.removes[0].path, "3.jpg");
+    EXPECT_EQ(delta.removes[0].type, Generic);
+}
+
+TEST(deltaList, simpleCopy) {
+    const std::vector<SimpleEntry> dest{SimpleEntry("1.jpg", "AAA", Generic),
+                                        SimpleEntry("2.jpg", "BBB", Generic),
+                                        SimpleEntry("3.jpg", "CCC", Generic)};
+
+    const std::vector<SimpleEntry> source{
+        SimpleEntry("1.jpg", "AAA", Generic),
+        SimpleEntry("2.jpg", "BBB", Generic),
+        SimpleEntry("3-new.jpg", "CCC", Generic),
+    };
+
+    const auto delta = getDelta(source, dest);
+
+    EXPECT_EQ(delta.copies.size(), 1);
+    EXPECT_EQ(delta.adds.size(), 0);
+    EXPECT_EQ(delta.removes.size(), 1);
+
+    EXPECT_EQ(delta.copies[0].source, "3.jpg");
+    EXPECT_EQ(delta.copies[0].destination, "3-new.jpg");
+
+    EXPECT_EQ(delta.removes[0].path, "3.jpg");
+}
+
+}  // namespace
