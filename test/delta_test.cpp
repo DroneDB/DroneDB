f2a1baa7deb2d544cde3e91d1377fd5d0ef2af2b
Luca Di Leo <admin@lucadileo.it>
2021-02-12 11:09:54 +0100

    Added tests

--- test/delta_test.cpp
+++ test/delta_test.cpp
@@ -2,9 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include <delta.h>
 
 #include "dbops.h"
+#include "delta.h"
 #include "exceptions.h"
 #include "gtest/gtest.h"
 #include "test.h"
@@ -15,14 +15,14 @@ namespace {
 using namespace ddb;
 
 TEST(deltaList, simpleAdd) {
-    const std::vector<SimpleEntry> dest{SimpleEntry("1.jpg", "AAA", Generic),
-                                        SimpleEntry("2.jpg", "BBB", Generic),
-                                        SimpleEntry("3.jpg", "CCC", Generic)};
+    const std::vector<SimpleEntry> dest{SimpleEntry("1.jpg", "AAA"),
+                                        SimpleEntry("2.jpg", "BBB"),
+                                        SimpleEntry("3.jpg", "CCC")};
 
-    const std::vector<SimpleEntry> source{SimpleEntry("1.jpg", "AAA", Generic),
-                                          SimpleEntry("2.jpg", "BBB", Generic),
-                                          SimpleEntry("3.jpg", "CCC", Generic),
-                                          SimpleEntry("4.jpg", "DDD", Generic)};
+    const std::vector<SimpleEntry> source{SimpleEntry("1.jpg", "AAA"),
+                                          SimpleEntry("2.jpg", "BBB"),
+                                          SimpleEntry("3.jpg", "CCC"),
+                                          SimpleEntry("4.jpg", "DDD")};
 
     const auto delta = getDelta(source, dest);
 
@@ -35,13 +35,13 @@ TEST(deltaList, simpleAdd) {
 }
 
 TEST(deltaList, simpleRemove) {
-    const std::vector<SimpleEntry> dest{SimpleEntry("1.jpg", "AAA", Generic),
-                                        SimpleEntry("2.jpg", "BBB", Generic),
-                                        SimpleEntry("3.jpg", "CCC", Generic)};
+    const std::vector<SimpleEntry> dest{SimpleEntry("1.jpg", "AAA"),
+                                        SimpleEntry("2.jpg", "BBB"),
+                                        SimpleEntry("3.jpg", "CCC")};
 
     const std::vector<SimpleEntry> source{
-        SimpleEntry("1.jpg", "AAA", Generic),
-        SimpleEntry("2.jpg", "BBB", Generic),
+        SimpleEntry("1.jpg", "AAA"),
+        SimpleEntry("2.jpg", "BBB"),
     };
 
     const auto delta = getDelta(source, dest);
@@ -55,14 +55,14 @@ TEST(deltaList, simpleRemove) {
 }
 
 TEST(deltaList, simpleCopy) {
-    const std::vector<SimpleEntry> dest{SimpleEntry("1.jpg", "AAA", Generic),
-                                        SimpleEntry("2.jpg", "BBB", Generic),
-                                        SimpleEntry("3.jpg", "CCC", Generic)};
+    const std::vector<SimpleEntry> dest{SimpleEntry("1.jpg", "AAA"),
+                                        SimpleEntry("2.jpg", "BBB"),
+                                        SimpleEntry("3.jpg", "CCC")};
 
     const std::vector<SimpleEntry> source{
-        SimpleEntry("1.jpg", "AAA", Generic),
-        SimpleEntry("2.jpg", "BBB", Generic),
-        SimpleEntry("3-new.jpg", "CCC", Generic),
+        SimpleEntry("1.jpg", "AAA"),
+        SimpleEntry("2.jpg", "BBB"),
+        SimpleEntry("3-new.jpg", "CCC"),
     };
 
     const auto delta = getDelta(source, dest);
@@ -80,26 +80,97 @@ TEST(deltaList, simpleCopy) {
 TEST(deltaList, edgeCase1) {
 
     const std::vector<SimpleEntry> dest{
-        SimpleEntry("a", "", Directory),
-        SimpleEntry("a/.ddb", "", Directory),
-        SimpleEntry("a/.ddb/dbase.sqlite", "BBB", Generic),
-        SimpleEntry("a/a.txt", "AAA", Generic),
-        SimpleEntry("a/b", "", Directory),
-        SimpleEntry("a/b/c.txt", "AAA", Generic)};
+        SimpleEntry("a"),
+        SimpleEntry("a/.ddb"),
+        SimpleEntry("a/.ddb/dbase.sqlite", "BBB"),
+        SimpleEntry("a/a.txt", "AAA"),
+        SimpleEntry("a/b"),
+        SimpleEntry("a/b/c.txt", "AAA")};
 
     const std::vector<SimpleEntry> source{
-        SimpleEntry("a", "", Directory),
-        SimpleEntry("a/.ddb", "", Directory),
-        SimpleEntry("a/.ddb/dbase.sqlite", "BBB", Generic),
-        SimpleEntry("a/a.txt", "AAA", Generic),
-        SimpleEntry("a/b", "", Directory),
-        SimpleEntry("a/b/c.txt", "AAA", Generic)};
+        SimpleEntry("a"),
+        SimpleEntry("a/.ddb"),
+        SimpleEntry("a/.ddb/dbase.sqlite", "BBB"),
+        SimpleEntry("a/a.txt", "AAA"),
+        SimpleEntry("a/b"),
+        SimpleEntry("a/b/c.txt", "AAA")};
 
     const auto delta = getDelta(source, dest);
 
     EXPECT_EQ(delta.copies.size(), 0);
     EXPECT_EQ(delta.adds.size(), 0);
     EXPECT_EQ(delta.removes.size(), 0);
+   
+}
+
+TEST(deltaList, complexTree1) {
+
+    const std::vector<SimpleEntry> dest{
+        SimpleEntry("1.jpg", "AAA"),
+        SimpleEntry("2.jpg", "BBB"),
+        SimpleEntry("3.jpg", "CCC"),
+        SimpleEntry("img"),
+        SimpleEntry("img/1.jpg", "AAA"),
+        SimpleEntry("img/2.jpg", "BBB"),
+        SimpleEntry("img/3.jpg", "CCC")};
+
+    const std::vector<SimpleEntry> source{
+        SimpleEntry("1.jpg", "CCC"),
+        SimpleEntry("2.jpg", "AAA"),
+        SimpleEntry("3.jpg", "BBB"),
+        SimpleEntry("cov"),
+        SimpleEntry("cov/1.jpg", "BBB"),
+        SimpleEntry("cov/2.jpg", "CCC"),
+        SimpleEntry("cov/3.jpg", "AAA")};
+
+    const auto delta = getDelta(source, dest);
+
+    auto expected = R"(
+    {
+  "adds": [
+    {
+      "path": "cov",
+      "type": 1
+    }
+  ],
+  "copies": [
+    ["3.jpg", "1.jpg"
+    ],
+    ["1.jpg", "2.jpg"
+    ],
+    ["2.jpg", "3.jpg"
+    ],
+    ["2.jpg", "cov/1.jpg"
+    ],
+    ["3.jpg", "cov/2.jpg"
+    ],
+    ["1.jpg", "cov/3.jpg"
+    ]
+  ],
+  "removes": [
+    {
+      "path": "img/3.jpg",
+      "type": 2
+    },
+    {
+      "path": "img/2.jpg",
+      "type": 2
+    },
+    {
+      "path": "img/1.jpg",
+      "type": 2
+    },
+    {
+      "path": "img",
+      "type": 1
+    }
+  ]
+}
+    )"_json;
+
+    json j = delta;
+    
+    EXPECT_EQ(j, expected);
    
 }
 
