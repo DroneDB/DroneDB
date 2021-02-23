aa7bdc2dbb1b71620cff2d5475fdc2dba75b6812
Luca Di Leo <ldileo@digipa.it>
2021-02-23 17:37:59 +0100

    Added workflow info comment

--- src/clone.cpp
+++ src/clone.cpp
@@ -21,6 +21,15 @@ void  clone(const std::string& target, const std::string& folder) {
     std::cout << "Target = " << target << std::endl;
     std::cout << "Folder = " << folder << std::endl;
 
+    // Workflow
+    // 1) Generate download url
+    // 1.1) If full url was not provided, generate it starting from tag (default remote is DEFAULT_REGISTRY)
+    // 2) Download zip in temp folder
+    // 3) Create target folder
+    // 3.1) If target folder already exists throw error
+    // 4) Unzip in target folder
+    // 5) Remove temp zip
+    // 6) Update sync information
 }
 
 }
