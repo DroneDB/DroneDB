9503621f7ed2ad0e7757d318baa1caf1a2e63c23
Luca Di Leo <ldileo@digipa.it>
2021-03-10 17:31:55 +0100

    Cosmetics

--- src/registryutils.cpp
+++ src/registryutils.cpp
@@ -5,19 +5,22 @@
 
 #include <url/Url.h>
 
-#include "exceptions.h"
 #include "constants.h"
+#include "exceptions.h"
 #include "utils.h"
 
-namespace ddb{
+namespace ddb {
 
-TagComponents RegistryUtils::parseTag(const std::string &tag, bool useInsecureRegistry){
+TagComponents RegistryUtils::parseTag(const std::string &tag,
+                                      bool useInsecureRegistry) {
     auto t = tag;
     utils::trim(t);
     utils::toLower(t);
 
     auto pos = t.rfind('/');
-    if (pos == std::string::npos) throw InvalidArgsException("Invalid tag: " + tag + " must be in organization/dataset format");
+    if (pos == std::string::npos)
+        throw InvalidArgsException("Invalid tag: " + tag +
+                                   " must be in organization/dataset format");
 
     TagComponents res;
     res.dataset = t.substr(pos + 1, t.length() - 1);
@@ -26,28 +29,35 @@ TagComponents RegistryUtils::parseTag(const std::string &tag, bool useInsecureRe
     pos = t.rfind('/');
     auto useDefaultRegistry = false;
 
-    if (pos == std::string::npos){
+    if (pos == std::string::npos) {
         res.organization = t;
         useDefaultRegistry = true;
-    }else{
+    } else {
         res.organization = t.substr(pos + 1, t.length() - 1);
     }
     t = t.substr(0, t.length() - res.organization.length() - 1);
 
-    if (t.empty() || useDefaultRegistry){
+    if (t.empty() || useDefaultRegistry) {
         // Use default registry URL
-        res.registryUrl = std::string(useInsecureRegistry ? "http://" : "https://") + DEFAULT_REGISTRY;
-    }else{
+        res.registryUrl =
+            std::string(useInsecureRegistry ? "http://" : "https://") +
+            DEFAULT_REGISTRY;
+    } else {
         // TODO: should we validate the URL more thoroughly?
-        const bool hasProto = t.find("http://") == 0 || t.find("https://") == 0 ;
-        if (hasProto) res.registryUrl = t;
-        else res.registryUrl = (useInsecureRegistry ? "http://" : "https://") + t;
+        const bool hasProto = t.find("http://") == 0 || t.find("https://") == 0;
+        if (hasProto)
+            res.registryUrl = t;
+        else
+            res.registryUrl =
+                (useInsecureRegistry ? "http://" : "https://") + t;
     }
 
     // Check that we haven't parsed the server part into the
     // organization name
-    if (res.organization.find("http://") == 0 || res.organization.find("https://") == 0){
-        throw InvalidArgsException("Invalid tag: " + tag + " missing dataset name");
+    if (res.organization.find("http://") == 0 ||
+        res.organization.find("https://") == 0) {
+        throw InvalidArgsException("Invalid tag: " + tag +
+                                   " missing dataset name");
     }
 
     const homer6::Url url(res.registryUrl);
@@ -57,22 +67,24 @@ TagComponents RegistryUtils::parseTag(const std::string &tag, bool useInsecureRe
     res.registryUrl = url.getScheme() + "://" + url.getHost();
 
     // Add port if needed
-    if ((url.getScheme() == "http" && url.getPort() != 80) || (url.getScheme() == "https" && url.getPort() != 443)) 
+    if ((url.getScheme() == "http" && url.getPort() != 80) ||
+        (url.getScheme() == "https" && url.getPort() != 443))
         res.registryUrl += ":" + std::to_string(url.getPort());
 
     return res;
 }
 
-Registry RegistryUtils::createFromTag(const std::string &tag, bool useInsecureRegistry){
+Registry RegistryUtils::createFromTag(const std::string &tag,
+                                      bool useInsecureRegistry) {
     const auto tc = parseTag(tag, useInsecureRegistry);
     return Registry(tc.registryUrl);
 }
 
-std::string TagComponents::tagWithoutUrl() const{
-    if (!organization.empty() && !dataset.empty()){
+std::string TagComponents::tagWithoutUrl() const {
+    if (!organization.empty() && !dataset.empty()) {
         return organization + "/" + dataset;
     }
     return "";
 }
 
-}
+}  // namespace ddb
