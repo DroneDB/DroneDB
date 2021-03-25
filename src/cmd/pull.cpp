1c29a84ddb721f6f2165d3fe7351ac5d468d3117
Luca Di Leo <ldileo@digipa.it>
2021-03-25 18:45:58 +0100

    Moved poll auth logic outside registry class

--- src/cmd/pull.cpp
+++ src/cmd/pull.cpp
@@ -24,13 +24,13 @@ void Pull::setOptions(cxxopts::Options& opts) {
     // clang-format off
         opts
             .positional_help("[args]")
-            .custom_help("pull [-f|--force] [remote]")
+            .custom_help("pull")
             .add_options()
             ("r,remote", "The remote Registry", cxxopts::value<std::string>()->default_value(""))
             ("f,force", "Forces the operation", cxxopts::value<bool>()->default_value("false"));
 
     // clang-format on
-    opts.parse_positional({"remote"});
+    //opts.parse_positional({"remote"});
 }
 
 std::string Pull::description() {
@@ -44,62 +44,10 @@ void Pull::run(cxxopts::ParseResult& opts) {
         auto remote = opts["remote"].as<std::string>();
         remote = remote.length() > 0 ? remote : DEFAULT_REGISTRY;
 
-        const auto currentPath = fs::current_path().string();
+        ddb::pull(remote, force);
 
-        ddb::TagManager manager(currentPath);
-        
-        ddb::TagComponents tag = ddb::RegistryUtils::parseTag(manager.getTag());
-
-        std::cout << "Pulling dataset '" << tag.organization << "/"
-                  << tag.dataset << "' from registry '" << tag.registryUrl
-                  << std::endl;
-
-        const AuthCredentials ac =
-            ddb::UserProfile::get()->getAuthManager()->loadCredentials(
-                tag.registryUrl);
-
-        ddb::Registry reg(tag.registryUrl);
-
-        try {
-            if (ac.empty()) {
-                const auto username = ddb::utils::getPrompt("Username: ");
-                const auto password = ddb::utils::getPass("Password: ");
-
-                if (reg.login(username, password).length() <= 0)
-                    throw ddb::AuthException("Cannot authenticate with " +
-                                             reg.getUrl());
-
-                ddb::UserProfile::get()->getAuthManager()->saveCredentials(
-                    tag.registryUrl, AuthCredentials(username, password));
-
-                reg.pull(currentPath, force, std::cout);
-
-            } else {
-                if (reg.login(ac.username, ac.password).length() <= 0)
-                    throw ddb::AuthException("Cannot authenticate with " +
-                                             reg.getUrl());
-
-                reg.pull(currentPath, force, std::cout);
-            }
-
-        } catch (const ddb::AuthException&) {
-            const auto username = ddb::utils::getPrompt("Username: ");
-            const auto password = ddb::utils::getPass("Password: ");
-
-            if (reg.login(username, password).length() > 0) {
-                ddb::UserProfile::get()->getAuthManager()->saveCredentials(
-                    tag.registryUrl, AuthCredentials(username, password));
-
-                reg.pull(currentPath, force, std::cout);
-
-            } else {
-                throw ddb::AuthException("Cannot authenticate with " +
-                                         reg.getUrl());
-            }
-        } catch (const ddb::AppException& e) {
-            throw e;
-        }
-    } catch (ddb::InvalidArgsException) {
+    } catch (ddb::InvalidArgsException ex) {
+        std::cout << ex.what() << std::endl;
         printHelp();
     }
 }
