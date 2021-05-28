2bc5569b0e8907e2b12dde09b054c3625da89c8e
Luca Di Leo <ldileo@digipa.it>
2021-05-28 19:53:23 +0200

    Added moveObj to dataset interface

--- nodejs/js/dataset.js
+++ nodejs/js/dataset.js
@@ -81,6 +81,10 @@ module.exports = class Dataset{
         return this.registry.deleteRequest(`${this.baseApi}/obj`, { path });
     }
 
+    async moveObj(source, dest) {
+        return this.registry.putRequest(`${this.baseApi}/obj`, { source, dest });
+    }
+
     async rename(slug){
         if (typeof slug !== "string") throw new Error(`Invalid slug ${slug}`);
         return this.registry.postRequest(`${this.baseApi}/rename`, { slug });
