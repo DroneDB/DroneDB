2a4a64814030eb472d80b6b0a05bce1f7b01e6fe
HeDo <ldileo@digipa.it>
2025-07-02 16:24:51 +0200

    Added ortho tests

--- tests/entry_test.cpp
+++ tests/entry_test.cpp
@@ -4,6 +4,8 @@
 
 #include "gtest/gtest.h"
 #include "entry.h"
+#include "test.h"
+#include "testarea.h"
 
 namespace
 {
@@ -32,5 +34,74 @@ namespace
         e.parsePolygonGeometry("[[[1, 2], [3,4]]]");
         EXPECT_EQ(e.polygon_geom.size(), 2);
     }
+
+    TEST(parseNZGD2000, Normal)
+    {
+        TestArea ta(TEST_NAME);
+        fs::path pc = ta.downloadTestAsset("https://github.com/DroneDB/test_data/raw/refs/heads/master/ortho/wro.tif",
+                                           "wro.tif");
+
+        Entry e;
+
+        parseEntry(pc, ta.getFolder(), e, false);
+
+        /* Expected values
+
+        Point Geometry: [[-41.06625411739, 175.4035257699, 0] ]
+Polygon Geometry: [[-41.06584339802, 175.4029416126, 0] [-41.06581965903, 175.4040791346, 0] [-41.06666483358, 175.4041099344, 0] [-41.06668857327, 175.4029723979, 0] [-41.06584339802, 175.4029416126, 0] ]
+
+        */
+
+        EXPECT_EQ(e.point_geom.size(), 1);
+        EXPECT_EQ(e.polygon_geom.size(), 5);
+        EXPECT_NEAR(e.point_geom.getPoint(0).x, -41.06625411739, 1e-9);
+        EXPECT_NEAR(e.point_geom.getPoint(0).y, 175.4035257699, 1e-9);
+        EXPECT_NEAR(e.polygon_geom.getPoint(0).x, -41.06584339802, 1e-9);
+        EXPECT_NEAR(e.polygon_geom.getPoint(0).y, 175.4029416126, 1e-9);
+        EXPECT_NEAR(e.polygon_geom.getPoint(1).x, -41.06581965903, 1e-9);
+        EXPECT_NEAR(e.polygon_geom.getPoint(1).y, 175.4040791346, 1e-9);
+        EXPECT_NEAR(e.polygon_geom.getPoint(2).x, -41.06666483358, 1e-9);
+        EXPECT_NEAR(e.polygon_geom.getPoint(2).y, 175.4041099344, 1e-9);
+        EXPECT_NEAR(e.polygon_geom.getPoint(3).x, -41.06668857327, 1e-9);
+        EXPECT_NEAR(e.polygon_geom.getPoint(3).y, 175.4029723979, 1e-9);
+        EXPECT_NEAR(e.polygon_geom.getPoint(4).x, -41.06584339802, 1e-9);
+        EXPECT_NEAR(e.polygon_geom.getPoint(4).y, 175.4029416126, 1e-9);
+
+    }
+
+        TEST(parseNZGD2000, Normal)
+    {
+        TestArea ta(TEST_NAME);
+        fs::path pc = ta.downloadTestAsset("https://github.com/DroneDB/test_data/raw/refs/heads/master/ortho/wro.tif",
+                                           "copr.tif");
+
+        Entry e;
+
+        parseEntry(pc, ta.getFolder(), e, false);
+
+        /* Expected values
+
+        Point Geometry: [[-41.06625411739, 175.4035257699, 0] ]
+Polygon Geometry: [[-41.06584339802, 175.4029416126, 0] [-41.06581965903, 175.4040791346, 0] [-41.06666483358, 175.4041099344, 0] [-41.06668857327, 175.4029723979, 0] [-41.06584339802, 175.4029416126, 0] ]
+
+        */
+
+        /*EXPECT_EQ(e.point_geom.size(), 1);
+        EXPECT_EQ(e.polygon_geom.size(), 5);
+        EXPECT_NEAR(e.point_geom.getPoint(0).x, -41.06625411739, 1e-9);
+        EXPECT_NEAR(e.point_geom.getPoint(0).y, 175.4035257699, 1e-9);
+        EXPECT_NEAR(e.polygon_geom.getPoint(0).x, -41.06584339802, 1e-9);
+        EXPECT_NEAR(e.polygon_geom.getPoint(0).y, 175.4029416126, 1e-9);
+        EXPECT_NEAR(e.polygon_geom.getPoint(1).x, -41.06581965903, 1e-9);
+        EXPECT_NEAR(e.polygon_geom.getPoint(1).y, 175.4040791346, 1e-9);
+        EXPECT_NEAR(e.polygon_geom.getPoint(2).x, -41.06666483358, 1e-9);
+        EXPECT_NEAR(e.polygon_geom.getPoint(2).y, 175.4041099344, 1e-9);
+        EXPECT_NEAR(e.polygon_geom.getPoint(3).x, -41.06668857327, 1e-9);
+        EXPECT_NEAR(e.polygon_geom.getPoint(3).y, 175.4029723979, 1e-9);
+        EXPECT_NEAR(e.polygon_geom.getPoint(4).x, -41.06584339802, 1e-9);
+        EXPECT_NEAR(e.polygon_geom.getPoint(4).y, 175.4029416126, 1e-9);*/
+
+    }
+
 
 }
