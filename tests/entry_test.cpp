f8eb0ee0b604ab20c52268e07ce6aa85a60a5a75
Luca Di Leo <ldileo@digipa.it>
2025-07-03 10:22:47 +0200

    Fixed entry test

--- tests/entry_test.cpp
+++ tests/entry_test.cpp
@@ -69,10 +69,10 @@ Polygon Geometry: [[-41.06584339802, 175.4029416126, 0] [-41.06581965903, 175.40
 
     }
 
-        TEST(parseNZGD2000, Normal)
+    TEST(parseEPSG4326, Normal)
     {
         TestArea ta(TEST_NAME);
-        fs::path pc = ta.downloadTestAsset("https://github.com/DroneDB/test_data/raw/refs/heads/master/ortho/wro.tif",
+        fs::path pc = ta.downloadTestAsset("https://github.com/DroneDB/test_data/raw/refs/heads/master/ortho/copr.tif",
                                            "copr.tif");
 
         Entry e;
@@ -81,25 +81,25 @@ Polygon Geometry: [[-41.06584339802, 175.4029416126, 0] [-41.06581965903, 175.40
 
         /* Expected values
 
-        Point Geometry: [[-41.06625411739, 175.4035257699, 0] ]
-Polygon Geometry: [[-41.06584339802, 175.4029416126, 0] [-41.06581965903, 175.4040791346, 0] [-41.06666483358, 175.4041099344, 0] [-41.06668857327, 175.4029723979, 0] [-41.06584339802, 175.4029416126, 0] ]
+Point Geometry: [[-119.8801994362, 34.40849815034, 0] ]
+Polygon Geometry: [[-119.8804248213, 34.40867109444, 0] [-119.8799862706, 34.40868142837, 0] [-119.8799740521, 34.40832520577, 0] [-119.8804126009, 34.40831487198, 0] [-119.8804248213, 34.40867109444, 0] ]
 
         */
 
-        /*EXPECT_EQ(e.point_geom.size(), 1);
+        EXPECT_EQ(e.point_geom.size(), 1);
         EXPECT_EQ(e.polygon_geom.size(), 5);
-        EXPECT_NEAR(e.point_geom.getPoint(0).x, -41.06625411739, 1e-9);
-        EXPECT_NEAR(e.point_geom.getPoint(0).y, 175.4035257699, 1e-9);
-        EXPECT_NEAR(e.polygon_geom.getPoint(0).x, -41.06584339802, 1e-9);
-        EXPECT_NEAR(e.polygon_geom.getPoint(0).y, 175.4029416126, 1e-9);
-        EXPECT_NEAR(e.polygon_geom.getPoint(1).x, -41.06581965903, 1e-9);
-        EXPECT_NEAR(e.polygon_geom.getPoint(1).y, 175.4040791346, 1e-9);
-        EXPECT_NEAR(e.polygon_geom.getPoint(2).x, -41.06666483358, 1e-9);
-        EXPECT_NEAR(e.polygon_geom.getPoint(2).y, 175.4041099344, 1e-9);
-        EXPECT_NEAR(e.polygon_geom.getPoint(3).x, -41.06668857327, 1e-9);
-        EXPECT_NEAR(e.polygon_geom.getPoint(3).y, 175.4029723979, 1e-9);
-        EXPECT_NEAR(e.polygon_geom.getPoint(4).x, -41.06584339802, 1e-9);
-        EXPECT_NEAR(e.polygon_geom.getPoint(4).y, 175.4029416126, 1e-9);*/
+        EXPECT_NEAR(e.point_geom.getPoint(0).x, -119.8801994362, 1e-9);
+        EXPECT_NEAR(e.point_geom.getPoint(0).y, 34.40849815034, 1e-9);
+        EXPECT_NEAR(e.polygon_geom.getPoint(0).x, -119.8804248213, 1e-9);
+        EXPECT_NEAR(e.polygon_geom.getPoint(0).y, 34.40867109444, 1e-9);
+        EXPECT_NEAR(e.polygon_geom.getPoint(1).x, -119.8799862706, 1e-9);
+        EXPECT_NEAR(e.polygon_geom.getPoint(1).y, 34.40868142837, 1e-9);
+        EXPECT_NEAR(e.polygon_geom.getPoint(2).x, -119.8799740521, 1e-9);
+        EXPECT_NEAR(e.polygon_geom.getPoint(2).y, 34.40832520577, 1e-9);
+        EXPECT_NEAR(e.polygon_geom.getPoint(3).x, -119.8804126009, 1e-9);
+        EXPECT_NEAR(e.polygon_geom.getPoint(3).y, 34.40831487198, 1e-9);
+        EXPECT_NEAR(e.polygon_geom.getPoint(4).x, -119.8804248213, 1e-9);
+        EXPECT_NEAR(e.polygon_geom.getPoint(4).y, 34.40867109444, 1e-9);
 
     }
 
