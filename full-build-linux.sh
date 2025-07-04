b1b3a015b01dc5b774be8df43f7205911dd61c90
Luca Di Leo <ldileo@digipa.it>
2025-07-04 18:29:01 +0200

    Added BUILD_TESTING to build scripts

--- full-build-linux.sh
+++ full-build-linux.sh
@@ -17,7 +17,7 @@ cd build
 export VCPKG_ROOT="../vcpkg"
 
 # Run CMake configure step
-cmake .. -DCMAKE_TOOLCHAIN_FILE="../vcpkg/scripts/buildsystems/vcpkg.cmake" -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_TOOLCHAIN_FILE="../vcpkg/scripts/buildsystems/vcpkg.cmake" -DCMAKE_BUILD_TYPE=Release -DCMAKE_BUILD_TESTING=ON
 
 # Build using detected CPU cores
 cmake --build . -- -j"$CPU_CORES"
