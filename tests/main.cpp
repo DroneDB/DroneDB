65a1ac65ce7243f45e39a8b23258de5663cefb6a
Luca Di Leo <ldileo@digipa.it>
2025-09-09 16:03:56 +0200

    Added test data cleanup switch

--- tests/main.cpp
+++ tests/main.cpp
@@ -6,14 +6,61 @@
 #include "logger.h"
 #include "ddb.h"
 #include "utils.h"
+#include "testarea.h"
+#include "testfs.h"
 #include <fstream>
+#include <iostream>
+#include <cstring>
 
 int main(int argc, char **argv)
 {
 
     DDBRegisterProcess(true);
 
-    // TODO: ability to clean previous TestAreas
+
+    bool cleanTestData = false;
+    for (int i = 1; i < argc; i++)
+    {
+        if (std::strcmp(argv[i], "--clean-testdata") == 0)
+        {
+            cleanTestData = true;
+            // Remove the argument from argv to prevent Google Test from seeing it
+            for (int j = i; j < argc - 1; j++)
+            {
+                argv[j] = argv[j + 1];
+            }
+            argc--;
+            break;
+        }
+        else if (std::strcmp(argv[i], "--help") == 0 || std::strcmp(argv[i], "-h") == 0)
+        {
+            std::cout << "DroneDB Test Runner\n";
+            std::cout << "Usage: ddbtest [options] [gtest_options]\n";
+            std::cout << "\nDroneDB specific options:\n";
+            std::cout << "  --clean-testdata    Clean all test areas and test filesystem caches before running tests\n";
+            std::cout << "\nGoogle Test options (run with --help for full list):\n";
+            std::cout << "  --gtest_filter=PATTERN    Run only tests matching the pattern\n";
+            std::cout << "  --gtest_also_run_disabled_tests    Run disabled tests too\n";
+            return 0;
+        }
+    }
+
+    // Clean test data if requested
+    if (cleanTestData)
+    {
+        std::cout << "Cleaning test data...\n";
+
+        TestArea::clearAll();
+        TestFS::clearCache("TestFS");
+
+        std::cout << "Test data cleanup completed.\n";
+
+        // If only cleaning was requested (no other arguments), exit
+        if (argc == 1)
+        {
+            return 0;
+        }
+    }
 
     auto time = std::chrono::system_clock::now();
 
--- tests/testarea.cpp
+++ tests/testarea.cpp
@@ -2,57 +2,51 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 #include "testarea.h"
-#include "logger.h"
-#include "exceptions.h"
-#include "mio.h"
+
 #include <cpr/cpr.h>
 
-TestArea::TestArea(const std::string &name, bool recreateIfExists)
-    : name(name)
-{
+#include "exceptions.h"
+#include "logger.h"
+#include "mio.h"
+
+TestArea::TestArea(const std::string& name, bool recreateIfExists) : name(name) {
     const auto root = getFolder();
     if (name.find("..") != std::string::npos)
         throw ddb::FSException("Cannot use .. in name");
 
-    if (recreateIfExists)
-    {
-        if (fs::exists(root))
-        {
+    if (recreateIfExists) {
+        if (fs::exists(root)) {
             LOGD << "Removing " << root;
             LOGD << "Removed " << fs::remove_all(root) << " files/folders";
         }
     }
 }
 
-fs::path TestArea::getPath(const fs::path &p)
-{
+fs::path TestArea::getPath(const fs::path& p) {
     return fs::temp_directory_path() / "ddb_test_areas" / fs::path(name) / p;
 }
 
-fs::path TestArea::getFolder(const fs::path &subfolder)
-{
+fs::path TestArea::getFolder(const fs::path& subfolder) {
     const fs::path root = fs::temp_directory_path() / "ddb_test_areas" / fs::path(name);
     auto dir = root;
     if (!subfolder.empty())
         dir = dir / subfolder;
 
-    if (!fs::exists(dir))
-    {
+    if (!fs::exists(dir)) {
         ddb::io::createDirectories(dir);
         LOGD << "Created test folder " << dir;
     }
     return dir;
 }
 
-fs::path TestArea::downloadTestAsset(const std::string &url, const std::string &filename, bool overwrite)
-{
+fs::path TestArea::downloadTestAsset(const std::string& url,
+                                     const std::string& filename,
+                                     bool overwrite) {
     fs::path destination = getFolder() / fs::path(filename);
 
-    if (fs::exists(destination))
-    {
+    if (fs::exists(destination)) {
         // Check if file exists but is empty (0 bytes)
-        if (fs::file_size(destination) == 0)
-        {
+        if (fs::file_size(destination) == 0) {
             // File exists but is empty, force overwrite
             overwrite = true;
             LOGD << "Found empty file at " << destination << ", forcing overwrite";
@@ -66,10 +60,22 @@ fs::path TestArea::downloadTestAsset(const std::string &url, const std::string &
 
     std::ofstream ofs(destination.string(), std::ios::binary);
     auto request = cpr::Download(ofs, cpr::Url{url}, cpr::VerifySsl(false));
+
     if (request.error)
-    {
         throw ddb::NetException("Failed to download " + url + " to " + destination.string());
-    }
 
     return destination;
+}
+
+void TestArea::clearAll() {
+    const fs::path testAreasRoot = fs::temp_directory_path() / "ddb_test_areas";
+    if (!fs::exists(testAreasRoot)) {
+        std::cout << "No test areas to clear\n";
+        return;
+    }
+
+    LOGD << "Removing all test areas from " << testAreasRoot;
+    auto removedCount = fs::remove_all(testAreasRoot);
+    LOGD << "Removed " << removedCount << " files/folders from test areas";
+    std::cout << "Cleared all test areas (" << removedCount << " items removed)\n";
 }
